4b88837d03b8d9bc4af9e91cf0e9baf14102c9d8
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-03-26 22:27:39 +0530

    update code

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const authRouter = require('./routes/auth.route')
-const accountRouter = require('./routes/account.routes')
 const cookieParser = require('cookie-parser') 
-const app = express();
 
+const app = express();
 app.use(express.json())
 app.use(cookieParser())
+
+const authRouter = require('./routes/auth.route')
+const accountRouter = require('./routes/account.routes')
+const transactionRoutes = require('./routes/transaction.routes')
+
 app.use("/api/auth", authRouter)
 app.use("/api/account", accountRouter)
 
--- src/controller/transaction.controller.js
+++ src/controller/transaction.controller.js
@@ -2,6 +2,7 @@ const transactionModel = require('../models/transaction.model')
 const ledgerModel = require("../models/ledger.model")
 const accoutModel = require("../models/account.model")
 const emailService = require("../Services/email.service")
+const { default: mongoose } = require('mongoose')
 
 async function createTransaction(req, res) {
     const {fromAccount, toAccount, amount, idempotencyKey} = req.body
@@ -30,10 +31,88 @@ async function createTransaction(req, res) {
 
     if(isTransactionAlreadyExists){
         if(isTransactionAlreadyExists.status === "COMPLETED"){
-            res.status(200).json({
+            return res.status(200).json({
                 message: "Transaction is already processed",
                 transaction: isTransactionAlreadyExists
             })
         }
+
+        if(isTransactionAlreadyExists.status === "PENDING"){
+            return res.status(200).json({
+                message: "Transaction is processed pending",
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "FAILED"){
+            return res.status(200).json({
+                message: "Transaction is processed Failed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "COMPLETED"){
+            return res.status(500).json({
+                message: "Transaction was reversed, please retry",
+            })
+        }
     }
+
+
+    //Check account status
+
+    if(fromUserAccount.status !== "ACTIVE" || toUserAccount !== "ACTIVE"){
+        return res.status(400).json({
+            message: "Both fromAccount and toAccount must be ACTIVE to process transaction"
+        })
+    }
+
+
+    // Derive sender balance from ledger
+
+    const balance = await fromUserAccount.getBalance()
+    if(balance < amount){
+       return res.status(400).json({
+            messaage: `Insufficient balance. Current balance is ${balance}. requested amount is ${amount}`
+        })
+    }
+
+
+
+    //Create transaction
+
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    const transaction = await transactionModel.create({
+        fromAccount,
+        toAccount,
+        amount,
+        idempotencyKey,
+        status: "PENDING"
+    }, {session})
+
+    const debitLedgerEntry = await ledgerModel.create({
+        account:  toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "DEBIT"
+    }, {session})
+
+    const creditLedgerEntry = await ledgerModel.create({
+        account:  toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "CREDIT"
+    }, {session})
+
+    transaction.status = "COMPLETED" 
+    await transaction.save({session})
+
+
+
+    await session.commitTransaction()
+    session.endSession()
+
+
+
 }
