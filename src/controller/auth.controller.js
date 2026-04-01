0fcae9c9bb8479f4196d80fded9ec4725dbab7a2
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-04-01 22:59:16 +0530

    update code

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -57,7 +57,7 @@ async function userLoginController(req,res){
    res.cookie("token", token)
 
    res.status(200).json({
-    message: "User Login successfully",
+    message: "User Login Successfully",
 
     user: {
         _id: user._id,
