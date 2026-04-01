8d9d6e7648c9e541df1bf86289047b8bbbb550b3
Anuragkulshrestha172315 <kulshresthaa204@gmail.com>
2026-04-01 23:00:13 +0530

    update code

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -76,7 +76,7 @@ async function userLogoutController(req,res){
 
     if(!token){
         return res.status(200).json({
-            message: "User logged out successfully"
+            message: "User logged out Successfully"
         })
     }
     res.cookie("token", "")
